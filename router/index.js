e6be4316fc836215d9b35b4a6eb0e95e30a8707e
Erick Muke <muke7881@gmail.com>
2024-10-10 03:57:59 -0600

    fix: Se cambia el pdf y imagen modelo relacional, se agrega padres al router principal

--- router/index.js
+++ router/index.js
@@ -1,9 +1,10 @@
 const express = require('express');
 const maestrosRouter = require('./maestrosRouter');
 const usuariosRouter = require('./usuariosRouter');
+const padresRouter = require('./padresRouter');
 
 const router = express.Router();
 
-router.use('/api/v1', maestrosRouter, usuariosRouter);
+router.use('/api/v1', maestrosRouter, usuariosRouter, padresRouter);
 
 module.exports = router;
