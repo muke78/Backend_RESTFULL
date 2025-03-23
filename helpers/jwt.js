eeab44884186254eab8b371adb8dbf5d8056141c
Erick Muke <muke7881@gmail.com>
2025-03-23 08:13:41 -0600

    feat: Se arregla el jwt de la importacion

--- helpers/jwt.js
+++ helpers/jwt.js
@@ -1,5 +1,5 @@
 import { addHour } from "@formkit/tempo";
-import jwt from "jwt-simple";
+import jwt from "jsonwebtoken";;
 
 process.loadEnvFile();
 
@@ -9,7 +9,7 @@ export const createToken = (user) => {
   // Duracion de 2 minutos para probar que el toke se expire correctamente
   // const expirationDate = addMinute(new Date(), 2);
 
-  const expirationDate = addHour(new Date(), 4);
+  const expirationDate = addHour(new Date(), 12);
 
   const expirationTime = Math.floor(expirationDate.getTime() / 1000);
 
@@ -23,5 +23,5 @@ export const createToken = (user) => {
     iat: Math.floor(Date.now() / 1000),
     exp: expirationTime,
   };
-  return jwt.encode(payload, secret);
+  return jwt.sign(payload, secret);
 };
