dfcc95556111884c6e2326257f39651b5aa202a5
Erick Muke <muke7881@gmail.com>
2025-04-27 08:24:01 -0600

    perf: aumentar límites de tamaño para JSON y URL en el middleware de express

--- server.js
+++ server.js
@@ -15,7 +15,8 @@ setupSwagger(app);
 
 // Middleware
 app.use(cors(corsOptions));
-app.use(express.json());
+app.use(express.json({ limit: "10mb" }));
+app.use(express.urlencoded({ extended: true, limit: "10mb" }));
 app.use(morgan("dev"));
 app.use(helmet());
 app.use(router);
