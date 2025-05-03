2aeb6cc4a10430eecddcaf0e0f731d474022c4f6
Erick Muke <muke7881@gmail.com>
2025-05-03 01:38:45 -0600

    feat: agregar endpoint para registrar nuevos usuarios

--- controllers/usuariosControllers.js
+++ controllers/usuariosControllers.js
@@ -163,6 +163,46 @@ const InsertarUsario = async (req, res) => {
   }
 };
 
+const RegistrarUsuario = async (req, res) => {
+  try {
+    const { nameUser, email, password, role } = req.body;
+
+    if (!nameUser || !email || !password || !role)
+      return methodIncorrect(req, res);
+
+    const [existingUser] = await connectionQuery(
+      `SELECT AccountType FROM users WHERE Email = ?`,
+      [email],
+    );
+
+    if (existingUser)
+      return methodConflicts(req, res, {
+        message: "El correo ya se encuentra registrado",
+      });
+
+    // Hashear la contraseña
+    const hashedPasword = await hashedArg.hash(password);
+    const queryInsert = `
+    INSERT INTO users (ID, NameUser, Email, Password, Role, AccountType, AccountStatus, LastLogin) 
+    VALUES (UUID(), ?, ?, ?, ?, "normal", "Inactivo", NULL)
+  `;
+    const queryParamsInsert = [nameUser, email, hashedPasword, role];
+    const result = await connectionQuery(queryInsert, queryParamsInsert);
+
+    // await insertTeacherBeforeUser(email);
+
+    // Verificar si se insertó correctamente
+    if (result.affectedRows > 0) {
+      const queryGetUser = `SELECT NameUser, Email, Role, AccountStatus FROM users WHERE Email = ?`;
+      const newUser = await connectionQuery(queryGetUser, [email]);
+
+      return methodCreated(req, res, newUser[0]);
+    }
+  } catch (error) {
+    methodError(req, res, error);
+  }
+};
+
 const EditarUsuario = async (req, res) => {
   try {
     const { nameUser, email, password, role, accountStatus, id } = req.body;
@@ -349,6 +389,7 @@ export default {
   ObtenerTodosLosUsuarios,
   BusquedaDeUsuarios,
   InsertarUsario,
+  RegistrarUsuario,
   EditarUsuario,
   EliminarUsuario,
   DeleteUserBulk,
