773fa5aef7814db12759667481919b561146a0da
Erick Muke <muke7881@gmail.com>
2025-07-12 05:01:10 -0600

    fix: Cambio de lowerCase a UpperCase para el query de eliminar usuarios

--- src/models/users/functions/delete.models.js
+++ src/models/users/functions/delete.models.js
@@ -1,18 +1,18 @@
 import { connectionQuery } from "../../../helpers/connection.helpers.js";
 
 export const validateFoundUserToEliminated = async (userId) => {
-  const query = `SELECT NameUser FROM users WHERE id = ?`;
+  const query = `SELECT NameUser FROM users WHERE ID = ?`;
   const params = [userId];
   return await connectionQuery(query, params);
 };
 
 export const deleteUser = async (userId) => {
-  const query = `DELETE FROM users WHERE id = ?`;
+  const query = `DELETE FROM users WHERE ID = ?`;
   const params = [userId];
   return await connectionQuery(query, params);
 };
 
 export const deleteUserBulk = async (placeholders, batch) => {
-  const query = `DELETE FROM users WHERE id IN (${placeholders})`;
+  const query = `DELETE FROM users WHERE ID IN (${placeholders})`;
   return await connectionQuery(query, batch);
 };
