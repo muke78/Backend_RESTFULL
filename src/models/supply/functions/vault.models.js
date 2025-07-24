b97611449a74e8e2ec1666067e06d82a1d1e60c5
Erick Muke <muke7881@gmail.com>
2025-07-24 03:49:11 -0600

    fix(supplies): Se restructura el catalogo completo de suministros y se corrige busqueda de activos

--- src/models/supply/functions/vault.models.js
+++ src/models/supply/functions/vault.models.js
@@ -1,7 +1,7 @@
 import { connectionQuery } from "../../../helpers/connection.helpers.js";
 
 export const vaultSupplyModel = async (supplyId) => {
-  const query = `UPDATE catsupplies SET Status = 'Inactivo' WHERE ID = ?`;
+  const query = `UPDATE cat_supplies SET status_id = 'cefdb296-61f5-11f0-a977-d843ae0db894' WHERE supplies_id = ?`;
   const params = [supplyId];
   await connectionQuery(query, params);
 };
--- src/services/supply/functions/search.services.js
+++ src/services/supply/functions/search.services.js
@@ -1,11 +1,30 @@
 import { connectionQuery } from "../../../helpers/connection.helpers.js";
 
 export const searchSupplyService = async (name) => {
-  let query = `SELECT * FROM catsupplies WHERE 1=1`;
+  let query = `SELECT 
+                  supplies_id,
+                  cat_supplier.name AS supplier,
+                  supply_units.name AS unit,
+                  cat_supplies.name,
+                  cat_supplies.description,
+                  quantity,
+                  purchase_date,
+                  expiry_date,
+                  cost,
+                  cat_status.name AS status
+              FROM
+                  cat_supplies
+                      INNER JOIN
+                  cat_supplier ON cat_supplier.supplier_id = cat_supplies.supplier_id
+                      INNER JOIN
+                  supply_units ON supply_units.unit_id = cat_supplies.unit_id
+                      INNER JOIN
+                  cat_status ON cat_status.status_id = cat_supplies.status_id WHERE 1=1`;
+
   const params = [];
 
   if (name) {
-    query += ` AND Name LIKE ?`;
+    query += ` AND cat_supplies.name LIKE ?`;
     params.push(`%${name}%`);
   } else {
     throw {
@@ -15,7 +34,7 @@ export const searchSupplyService = async (name) => {
       details: "El campo de nombre es obligatorio para realizar busquedas",
     };
   }
-
+  console.log(query);
   const result = await connectionQuery(query, params);
 
   if (result.length === 0) {
