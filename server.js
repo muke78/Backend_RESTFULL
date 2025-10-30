51a59e34fb9ad2ef57187aea5758bf49d2feacf3
Erick Muke <muke7881@gmail.com>
2025-10-30 00:06:42 -0600

    fix(limit): Se limito el tamano del body de carga sobre la informacion enviada al backend

--- server.js
+++ server.js
@@ -22,8 +22,8 @@ setupSwagger(app);
 
 // ✅ 1. Middlewares básicos primero
 app.use(cors(corsOptions));
-app.use(express.json({ limit: "10mb" }));
-app.use(express.urlencoded({ extended: true, limit: "10mb" }));
+app.use(express.json({ limit: "1mb" }));
+app.use(express.urlencoded({ extended: true, limit: "1mb" }));
 app.use(morgan("dev"));
 app.use(helmet());
 app.use(cookieParser());
